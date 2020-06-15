e51fa0eba11cdf47d2e850731e2ee916e28230e3
TAYYAB DOST <tayyabdost@gmail.com>
2020-06-15 17:38:26 +0500

    Successfully Implemented Product Key functionality

--- LicenseKey_Functionality/LicenseKey_Functionality/LicenseForm.Designer.cs
+++ LicenseKey_Functionality/LicenseKey_Functionality/LicenseForm.Designer.cs
@@ -53,20 +53,20 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(25, 100);
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(46, 97);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(101, 31);
+			this.label2.Size = new System.Drawing.Size(80, 25);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Name: ";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(20, 155);
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(31, 157);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(177, 31);
+			this.label3.Size = new System.Drawing.Size(141, 25);
 			this.label3.TabIndex = 3;
 			this.label3.Text = "Product Key: ";
 			// 
@@ -75,44 +75,46 @@
 			this.pictureBox1.Image = global::LicenseKey_Functionality.Properties.Resources.licenseKey;
 			this.pictureBox1.Location = new System.Drawing.Point(563, 12);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(225, 268);
+			this.pictureBox1.Size = new System.Drawing.Size(225, 236);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 4;
 			this.pictureBox1.TabStop = false;
 			// 
 			// LicenseKeyMaskedTextBox
 			// 
-			this.LicenseKeyMaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.LicenseKeyMaskedTextBox.Location = new System.Drawing.Point(203, 151);
+			this.LicenseKeyMaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LicenseKeyMaskedTextBox.Location = new System.Drawing.Point(178, 155);
+			this.LicenseKeyMaskedTextBox.Mask = ">AAAAA-AAAAA-AAAAA-AAAAA-AAAAA";
 			this.LicenseKeyMaskedTextBox.Name = "LicenseKeyMaskedTextBox";
-			this.LicenseKeyMaskedTextBox.Size = new System.Drawing.Size(334, 40);
-			this.LicenseKeyMaskedTextBox.TabIndex = 5;
+			this.LicenseKeyMaskedTextBox.Size = new System.Drawing.Size(369, 29);
+			this.LicenseKeyMaskedTextBox.TabIndex = 1;
 			// 
 			// NameTextBox
 			// 
-			this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.NameTextBox.Location = new System.Drawing.Point(203, 91);
+			this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.NameTextBox.Location = new System.Drawing.Point(178, 95);
 			this.NameTextBox.Name = "NameTextBox";
-			this.NameTextBox.Size = new System.Drawing.Size(334, 40);
-			this.NameTextBox.TabIndex = 6;
+			this.NameTextBox.Size = new System.Drawing.Size(369, 29);
+			this.NameTextBox.TabIndex = 0;
 			// 
 			// VerifyButton
 			// 
-			this.VerifyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.VerifyButton.Location = new System.Drawing.Point(215, 218);
+			this.VerifyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.VerifyButton.Location = new System.Drawing.Point(196, 203);
 			this.VerifyButton.Name = "VerifyButton";
-			this.VerifyButton.Size = new System.Drawing.Size(158, 50);
-			this.VerifyButton.TabIndex = 7;
+			this.VerifyButton.Size = new System.Drawing.Size(158, 45);
+			this.VerifyButton.TabIndex = 2;
 			this.VerifyButton.Text = "Verify";
 			this.VerifyButton.UseVisualStyleBackColor = true;
+			this.VerifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
 			// 
 			// ExitButton
 			// 
-			this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ExitButton.Location = new System.Drawing.Point(379, 218);
+			this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ExitButton.Location = new System.Drawing.Point(376, 203);
 			this.ExitButton.Name = "ExitButton";
-			this.ExitButton.Size = new System.Drawing.Size(158, 50);
-			this.ExitButton.TabIndex = 8;
+			this.ExitButton.Size = new System.Drawing.Size(158, 45);
+			this.ExitButton.TabIndex = 3;
 			this.ExitButton.Text = "Exit";
 			this.ExitButton.UseVisualStyleBackColor = true;
 			this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
@@ -121,7 +123,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 292);
+			this.ClientSize = new System.Drawing.Size(800, 266);
 			this.Controls.Add(this.ExitButton);
 			this.Controls.Add(this.VerifyButton);
 			this.Controls.Add(this.NameTextBox);
@@ -134,8 +136,9 @@
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "LicenseForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "LicenseForm";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LicenseForm_FormClosing);
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LicenseForm_FormClosed);
 			this.Load += new System.EventHandler(this.LicenseForm_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
--- LicenseKey_Functionality/LicenseKey_Functionality/LicenseForm.cs
+++ LicenseKey_Functionality/LicenseKey_Functionality/LicenseForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +14,16 @@ namespace LicenseKey_Functionality
 {
 	public partial class LicenseForm : Form
 	{
+		private static string connectionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
 		public int TotalDaysLeft { get; set; }
 		public LicenseForm()
 		{
-			InitializeComponent();			
+			InitializeComponent();
 		}
 
 		private void LicenseForm_Load(object sender, EventArgs e)
 		{
+			NameTextBox.Focus();
 			TrialMessageLabel.Text = "You have " + TotalDaysLeft + " Days left in your Trial Period";
 
 			if (TotalDaysLeft > 0)
@@ -30,25 +34,125 @@ namespace LicenseKey_Functionality
 
 		private void ExitButton_Click(object sender, EventArgs e)
 		{
-			CheckDaysOnClosing(true);
+			CheckDaysOnClosing();
 		}
 
-		private void CheckDaysOnClosing(bool closeForm)
+		private void CheckDaysOnClosing()
 		{
 			//Program.mainForm.Hide();
 			if (TotalDaysLeft == 0)
 			{
 				Application.Exit();
 			}
-			if (closeForm)
+			else
 			{
 				this.Close();
 			}
 		}
 
-		private void LicenseForm_FormClosing(object sender, FormClosingEventArgs e)
+
+		private void VerifyButton_Click(object sender, EventArgs e)
 		{
-			CheckDaysOnClosing(false);
+			if (IsValidated())
+			{
+				UpdateDetails();
+			}
+		}
+
+		private void UpdateDetails()
+		{
+			using (SqlConnection sql = new SqlConnection(connectionString))
+			{
+				using (SqlCommand cmd = new SqlCommand("Update License set Name=@Name,ProductKey=@ProductKey", sql))
+				{
+					sql.Open();
+
+					cmd.Parameters.AddWithValue("@Name", NameTextBox.Text);
+					cmd.Parameters.AddWithValue("@ProductKey", LicenseKeyMaskedTextBox.Text);
+
+					cmd.ExecuteNonQuery();
+					MessageBox.Show("Product Key is valid. Now yo can use the system for lifetime.\nThanks for chosing us.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					this.Close();
+				}
+			}
+			
+		}
+
+		private bool IsValidated()
+		{
+			if (NameTextBox.Text.Trim() == String.Empty)
+			{
+				MessageBox.Show("Name is required.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				NameTextBox.Focus();
+				return false;
+			}
+			if (!LicenseKeyMaskedTextBox.MaskCompleted)
+			{
+				MessageBox.Show("Product Key should be 25 characters long.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				LicenseKeyMaskedTextBox.Focus();
+				return false;
+			}
+			else
+			{
+				bool isValid = ValidateProductKey(LicenseKeyMaskedTextBox.Text);
+				if (!isValid)
+				{
+					MessageBox.Show("Invalid Product Key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					LicenseKeyMaskedTextBox.Focus();
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private bool ValidateProductKey(string productKey)
+		{
+
+			//15 Different of Product Keys
+
+			//3T9A0-Y4X6I-B9G7D-S6Q2O-U1K48
+			//9T1AL-Y7C6J-B5P3D-S2Z4O-M5V9X
+			//2T5A1-YZX37-B8F9D-S4W3O-O1H6N
+			//1T7A0-Y3L7R-B5Q9D-S4X3O-J6C4K
+			//4T3AX-Y9Z7L-B2R3D-S1P8O-K0U1X
+
+
+			//2T5A1-Y7C6J-B5Q9D-S2Z4O-J6C4K
+			//9T1AL-Y9Z7L-B4Q1D-S6Q2O-O1H6N
+			//4T3AX-Y3L7R-B2R3D-S4X3O-M5V9X
+			//1T7A0-YZX37-B9G7D-S3R4O-U1K7J
+			//3T9A0-Y4X6I-B8F9D-S2N8O-K0U1X
+
+
+			//2T5A1-Y3L7R-B2R3D-S6Q2O-O1H6N
+			//9T1AL-YZX37-B9G7D-S2Z4O-J6C4K
+			//3T9A0-Y7C6J-B5Q9D-S3R4O-U1K7J
+			//1T7A0-Y9Z7L-B4Q1D-S4W3O-M5V9X
+			//0T8AI-Y1H9I-B3Z7D-S4V9O-X0E6W
+
+			StringBuilder builder = new StringBuilder();
+
+			builder.Append(productKey.Substring(1, 1));	  //T
+			builder.Append(productKey.Substring(3, 1));	  //A
+			builder.Append(productKey.Substring(6, 1));	  //Y
+			builder.Append(productKey.Substring(6, 1));	  //Y
+			builder.Append(productKey.Substring(3, 1));	  //A
+			builder.Append(productKey.Substring(12, 1));  //B
+			builder.Append(productKey.Substring(16, 1));  //D
+			builder.Append(productKey.Substring(22, 1));  //O
+			builder.Append(productKey.Substring(18, 1));   //S
+			builder.Append(productKey.Substring(1, 1));   //T
+
+			if (builder.ToString() != "TAYYABDOST")
+			{
+				return false;
+			}
+			return true;
+		}
+
+		private void LicenseForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			CheckDaysOnClosing();
 		}
 	}
 }
